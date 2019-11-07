f1e40388b1a8437d6f0ed6dcde1c868f1b855758
Dawid <Dawidos01111@gmail.com>
2019-11-07 21:28:39 +0100

    1 commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// hihihihih idziemy zbierać śnieg 
+// Gorące paszteciki barszczyk czerwony
+// Okej to idziemy zjeść sobie ;)
+// mam damplingi 
+namespace SA
+{
+
+    public class GameManager : MonoBehaviour
+    {
+        public int maxHeigt = 15;
+        public int maxWidth = 17;
+        public Color color1;
+        public Color color2;
+
+        GameObject mapObject;
+        SpriteRenderer mapRender;
+        private void Start()
+        {
+            CreateMap();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        void CreateMap()
+        {
+            mapObject = new GameObject("Map");
+            mapRender = mapObject.AddComponent<SpriteRenderer>();
+            Texture2D txt = new Texture2D(maxWidth, maxHeigt); // Stworzenie naszej mapy gry o maksymalnych rozmiarach
+
+            // Tworzenie mapy siatki 
+            for (int x = 0; x < maxWidth; x++)
+            {
+                for (int y = 0; y < maxHeigt; y++)
+                {
+                    if (x % 2 != 0)
+                    {
+                        if (y % 2 != 0)
+                        {
+                            txt.SetPixel(x, y, color1);
+                        }
+                        else
+                        {
+                            txt.SetPixel(x, y, color2);
+                        }
+
+                    }
+                    else
+                    {
+                        if (y % 2 != 0)
+                        {
+                            txt.SetPixel(x, y, color2);
+                        }
+                        else
+                        {
+                            txt.SetPixel(x, y, color1);
+                        }
+                    }
+
+                }
+            }
+            txt.filterMode = FilterMode.Point;
+            txt.Apply();
+            Rect rect = new Rect(0, 0, maxWidth, maxHeigt);
+            Sprite sprite = Sprite.Create(txt, rect, Vector2.one * .5f, 1, 0, SpriteMeshType.FullRect);
+            mapRender.sprite = sprite;
+        }
+    }
+
+}
+// Tworzenie mapy
